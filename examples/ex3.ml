dd37b778ce31ead0e80c2eb6ea0c690bb6d52b31
Oliver Gu <gu.oliver@yahoo.com>
2015-04-05 02:01:06 +0200

    Tweak example after signature change of candlestick series

--- examples/ex3.ml
+++ examples/ex3.ml
@@ -2,31 +2,37 @@ open Core.Std
 open Gnuplot
 
 let () =
+  let gen_times ~num_bars =
+    let now = Time.now () in
+    List.init num_bars ~f:(fun i ->
+      Time.sub now Time.Span.(scale day (float i)))
+  in
   (* Generate random candlestick bars. *)
-  let generate_bars ~num_bars =
-    let next_bar tm cl =
-      let tm = Time.add tm Time.Span.day in
+  let gen_bars ~num_bars =
+    let next_bar cl =
       let op = cl +. (Random.float 1. -. 0.5) /. 2. in
       let hi = op +. Random.float 1. /. 5. in
       let lo = op -. Random.float 1. /. 5. in
       let cl = (lo +. hi) /. 2. in
-      (tm, op, hi, lo, cl)
+      op, hi, lo, cl
     in
-    let rec loop n_bars bars ((tm, _, _, _, cl) as bar) =
+    let rec loop n_bars bars (( _, _, _, cl) as bar) =
       if n_bars = 0 then
         bars
       else
-        loop (n_bars - 1) (bar :: bars) (next_bar tm cl)
+        loop (n_bars - 1) (bar :: bars) (next_bar cl)
     in
-    let tm = Time.(sub (now ()) (Span.(scale day (float num_bars)))) in
     let op = 100. in
     let hi = op +. Random.float 1. /. 5. in
     let lo = op -. Random.float 1. /. 5. in
     let cl = (lo +. hi) /. 2. in
-    loop num_bars [] (tm, op, hi, lo, cl) |> List.rev
+    loop num_bars [] (op, hi, lo, cl)
+  in
+  let gen_data () =
+    List.zip_exn (gen_times ~num_bars:100) (gen_bars ~num_bars:100)
   in
   let gp = Gp.create () in
   (* Plot a random candlestick chart. *)
-  Gp.plot gp (Series.candlesticks (generate_bars ~num_bars:100) ~title:"chart");
+  Gp.plot gp (Series.candlesticks (gen_data ()) ~title:"chart");
   Gp.close gp
 ;;
